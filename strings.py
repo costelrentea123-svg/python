952a5f1ac4b5b45eab9fe8917a62f5ac3405a9c4
costelrentea123-svg <costelrentea123@gmail.com>
2025-08-03 21:35:27 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,16 @@
+character_name = "Costel"
+character_age = "25"
+is_male= True
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print(" ")
+character_name = "Andrei"
+character_age = "30"
+character_hobby = "footbal"
+var_food = "pizza"
+print("He really liked the name " + character_name + ",")
+print("but didnt like being " + character_age + ".")
+print("he likes to play " + character_hobby + ".")
+print("His favorite food is: " + var_food)
+
