13b5b11a00d48c6d8c780c7e73974363e5cd1b2b
costelr <costelrentea123@gmail.com>
2025-08-05 15:23:16 +0300

    tuples

--- tuples.py
+++ tuples.py
@@ -1,3 +1,9 @@
 
-coordinates = (4,5)
-print(coordinates)
+coordinates = [(4,5), (6,7), (80,35)]
+print(coordinates[0])
+
+print(f"Coordinates are the following: {coordinates[1]}")
+
+# or
+
+print("Coordinates are the following: " + str(coordinates[2]))
