1d5f54c694e8a81a325e09e2e42e283ed7e3aca3
costelr <costelrentea123@gmail.com>
2025-08-05 12:45:04 +0300

    share

--- list_functions.py
+++ list_functions.py
@@ -1,6 +1,16 @@
-print("cffff")
+lucky_numbers = [4, 8, 15, 16, 56, 23 ]
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Tim", "Jim", "Jim", "Jim"]
 
 
-var = 1
-print(var)
-print("The value is")
+friends.extend(lucky_numbers)
+friends.append("John")
+
+friends.insert(2, "Paul")
+
+print(friends)
+print(friends.index("Karen"))
+print(friends.count("Jim"))
+
+lucky_numbers.sort()
+print(lucky_numbers)
--- lists.py
+++ lists.py
@@ -3,3 +3,12 @@ friends[1]= "Ion"
 
 print(friends[1])
 print(friends[1:4])
+
+var = 1
+print(var)
+print("The value is")
+
+def phone_bill(name, amount, date):
+    print(f"Hello {name}, your bill is ${amount}. Due date is {date}.")
+
+phone_bill("Costel", 94.5, str("10.10.2025"))
